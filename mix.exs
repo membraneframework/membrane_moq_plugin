1761200bd6c66cf06a885c1ce31f3f993a681b5b
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-21 11:45:38 +0200

    Remove LICENCE from docs spec

--- mix.exs
+++ mix.exs
@@ -71,7 +71,7 @@ defmodule Membrane.Template.Mixfile do
   defp docs do
     [
       main: "readme",
-      extras: ["README.md", "LICENSE"],
+      extras: ["README.md"],
       formatters: ["html"],
       source_ref: "v#{@version}",
       nest_modules_by_prefix: [Membrane.Template]

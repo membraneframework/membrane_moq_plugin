6477d89e9ba327ab2cf4b1016455abd9ea2e4d76
Jakub Migdał <j.migdal42@gmail.com>
2026-06-16 15:08:14 +0200

    Change unknown message log level to `warning`

--- lib/sink.ex
+++ lib/sink.ex
@@ -125,7 +125,7 @@ defmodule Membrane.MoQ.Sink do
 
   @impl true
   def handle_info(msg, _ctx, state) do
-    Membrane.Logger.info("Unknown message: #{inspect(msg)}")
+    Membrane.Logger.warning("Unknown message: #{inspect(msg)}")
     {[], state}
   end
 

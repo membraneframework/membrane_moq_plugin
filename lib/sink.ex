8d8aab4e3e0af201e660e9b6caa8b34df16ce00e
Jakub Migdał <j.migdal42@gmail.com>
2026-07-13 20:22:07 +0200

    Restrict AAC/Opus formats according to upstream

--- lib/sink.ex
+++ lib/sink.ex
@@ -20,12 +20,16 @@ defmodule Membrane.MoQ.Sink do
   alias ExMoQ.Native
   alias Membrane.MoQ.TrackFormat
 
+  # MoQ explicitly expects raw AAC/Opus frames:
+  # https://github.com/moq-dev/moq/blob/21da717b92c73d8b3643a0028e7554b8b1149943/rs/moq-mux/src/codec/aac/mod.rs#L4
+  # https://github.com/moq-dev/moq/blob/21da717b92c73d8b3643a0028e7554b8b1149943/rs/moq-mux/src/codec/opus/mod.rs#L4
+
   def_input_pad :input,
     availability: :on_request,
     accepted_format:
       any_of(
-        Membrane.AAC,
-        Membrane.Opus,
+        %Membrane.AAC{encapsulation: :none},
+        %Membrane.Opus{self_delimiting?: false},
         %Membrane.H264{stream_structure: ss} when Membrane.H264.is_avc(ss),
         %Membrane.H265{stream_structure: ss} when Membrane.H265.is_hvc(ss)
       ),

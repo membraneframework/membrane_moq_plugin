bfd5f2358eb8d48ad6bab70de5862a97cec3ac2c
Jakub Migdał <j.migdal42@gmail.com>
2026-07-20 23:11:41 +0200

    Bump min Elixir version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Membrane.MoQ.Mixfile do
     [
       app: :membrane_moq_plugin,
       version: @version,
-      elixir: "~> 1.13",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),

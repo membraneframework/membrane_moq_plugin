acc04a70f912cce722f8bc79bc0d02f498b59fd4
Jakub Migdał <j.migdal42@gmail.com>
2026-07-22 17:31:16 +0200

    Remove gunk from sink test

--- test/sink_test.exs
+++ test/sink_test.exs
@@ -5,25 +5,25 @@ defmodule Membrane.MoQ.SinkTest do
   import Membrane.ChildrenSpec
   import Membrane.Testing.Assertions
   alias Membrane.MoQ.Test.Relay
-  alias Membrane.Testing.Pipeline
+  alias Membrane.Testing
 
   test "sink crashes during setup when given a malformed url" do
-    pipeline = Pipeline.start_supervised!()
+    pipeline = Testing.Pipeline.start_supervised!()
     ref = Process.monitor(pipeline)
 
     spec = child(:sink, %Membrane.MoQ.Sink{url: "not a url", broadcast: "test"})
-    :ok = Pipeline.execute_actions(pipeline, spec: spec)
+    :ok = Testing.Pipeline.execute_actions(pipeline, spec: spec)
 
     assert_receive {:DOWN, ^ref, :process, ^pipeline, reason}, 5_000
     assert {:membrane_child_crash, :sink, _error} = reason
   end
 
   @tag :integration
-  test "a session disconnect after setup notifies the parent instead of crashing" do
+  test "a session disconnect after setup notifies the parent" do
     relay = Relay.ensure!()
 
     pipeline =
-      Pipeline.start_supervised!(
+      Testing.Pipeline.start_link_supervised!(
         spec:
           child(:sink, %Membrane.MoQ.Sink{
             url: relay.url,
@@ -32,14 +32,11 @@ defmodule Membrane.MoQ.SinkTest do
           })
       )
 
-    ref = Process.monitor(pipeline)
-
     assert_child_setup_completed(pipeline, :sink, 10_000)
 
-    sink_pid = Pipeline.get_child_pid!(pipeline, :sink)
+    sink_pid = Testing.Pipeline.get_child_pid!(pipeline, :sink)
     send(sink_pid, {:moq_disconnected, "injected session close"})
 
-    assert_pipeline_notified(pipeline, :sink, {:disconnected, "injected session close"}, 5_000)
-    refute_receive {:DOWN, ^ref, :process, ^pipeline, _reason}, 500
+    assert_pipeline_notified(pipeline, :sink, {:disconnected, "injected session close"})
   end
 end

489edf2e0ce0ad56d1b6ecbdf1d5c3b1356ba758
Jakub Migdał <j.migdal42@gmail.com>
2026-09-03 19:26:00 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -46,8 +46,8 @@ defmodule Membrane.MoQ.Mixfile do
       {:membrane_aac_format, "~> 0.8.0"},
       {:membrane_opus_format, "~> 0.3.0"},
       # TODO: change to released version once it's public
-      {:ex_moq, github: "membraneframework/ex_moq", branch: "kidq330/group_loss_fixed"},
-      {:muontrap, ">= 0.0.0", only: :test},
+      {:ex_moq, github: "membraneframework/ex_moq", branch: "kidq330/initial"},
+      {:muontrap, "~> 1.8", only: :test},
       {:membrane_aac_plugin, "~> 0.19", only: :test},
       {:membrane_file_plugin, "~> 0.17", only: :test},
       {:membrane_realtimer_plugin, "~> 0.9", only: :test},

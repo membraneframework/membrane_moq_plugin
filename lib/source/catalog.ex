6f538a7ea8a96705564fd8a03e6f94fcf2aa821b
Jakub Migdał <j.migdal42@gmail.com>
2026-09-03 19:25:42 +0200

    Use new Native.renditions() type alias

--- lib/source/catalog.ex
+++ lib/source/catalog.ex
@@ -10,12 +10,12 @@ defmodule Membrane.MoQ.Source.Catalog do
         }
 
   @type t :: %__MODULE__{
-          renditions: %{Native.track() => Native.track_format()}
+          renditions: Native.renditions()
         }
 
   defstruct renditions: %{}
 
-  @spec update(t(), %{Native.track() => Native.track_format()}) :: {diff(), t()}
+  @spec update(t(), Native.renditions()) :: {diff(), t()}
   def update(catalog, new) do
     old = catalog.renditions
 
@@ -31,6 +31,6 @@ defmodule Membrane.MoQ.Source.Catalog do
     {%{removed: removed, added: added, changed: changed}, %{catalog | renditions: new}}
   end
 
-  @spec rendition(t(), Native.track()) :: Native.track_format() | nil
+  @spec rendition(t(), Native.track()) :: Native.track_format() | :unrecognized | nil
   def rendition(catalog, track), do: catalog.renditions[track]
 end
